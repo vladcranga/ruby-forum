6bd62beaef5a18e3962fd49afb7d766a6422a4dd
Vlad M. Cranga <vladcranga@yahoo.com>
2021-08-15 00:08:56 +0300

    auth system improvements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,11 @@ class PostsController < ApplicationController
     end 
 
     def new
-     @post = Post.new
+      @post = current_user.posts.build
     end 
 
     def create
-     @post = Post.new(post_params)
+     @post = current_user.posts.build(post_params)
      if @post.save
         redirect_to @post
        else
@@ -44,5 +44,6 @@ class PostsController < ApplicationController
     end
 
     before_action :find_post, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
 
 end
